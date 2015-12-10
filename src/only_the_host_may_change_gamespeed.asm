23ee94775081749e13e58470f64154ca8a7325d2
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2015-12-10 09:57:03 +0100

    macro example 2

--- src/only_the_host_may_change_gamespeed.asm
+++ src/only_the_host_may_change_gamespeed.asm
@@ -1,19 +1,11 @@
 %include "macros/patch.inc"
 %include "macros/datatypes.inc"
 
-cglobal IsHost
+gbool IsHost, false
 
-@CLEAR 0x004AA5D2, 0x90, 0x004AA5DB
-@LJMP 0x004AA5D2, _only_the_host_may_change_gamespeed
-
-section .bss
-    IsHost resb 1
-
-section .text
-
-_only_the_host_may_change_gamespeed:
+hack 0x004AA5D2, 0x004AA5DB ; only the host may change gamespeed
 	cmp byte[0x7E4580], 1
 	jnz 0x004AA64B
 	cmp byte[IsHost], 1
 	jnz 0x004AA651
-	jmp 0x004AA5DB
+	jmp hackend
