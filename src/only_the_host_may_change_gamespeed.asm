bdb9e0b241211d5bd7d4016336caf41202f54cb0
Daniel Keeton Jr <dakeeton@gmail.com>
2015-12-11 04:45:20 -0600

    Assume the user IsHost for offline mode. (spawner will overwrite this value anyway).

--- src/only_the_host_may_change_gamespeed.asm
+++ src/only_the_host_may_change_gamespeed.asm
@@ -1,10 +1,10 @@
 %include "macros/patch.inc"
 %include "macros/datatypes.inc"
 
-gbool IsHost, false
+gbool IsHost, true
 
 hack 0x004AA5D2, 0x004AA5DB ; only the host may change gamespeed
-	cmp byte[0x7E4580], 1
+        cmp byte[0x7E4580], 1
 	jnz 0x004AA64B
 	cmp byte[IsHost], 1
 	jnz 0x004AA651
