85b27c117a10ad5d702a7b72aaeb390b62cf1d1f
Daniel Keeton Jr <dakeeton@gmail.com>
2016-02-03 07:03:12 -0600

    Adding mapsnapshot to debugging functions.

--- src/debugging_help.asm
+++ src/debugging_help.asm
@@ -4,6 +4,7 @@
 
 ;;; Increase player disconnection timeout to max
 @SET 0x00707FC4, dd 0x7FFFFFFF
+sstring str_MapSnapshot, "MapSnapshot.map"
 
 hack 0x004E9D20, 0x004E9D28
         xor     edx, edx
