4d614dec75e0670a36056fbc537d13a32f389af6
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2016-03-14 13:11:03 +0100

    crash fix for triggers with singleplayer house owners

--- src/singleplayer_objects_on_multiplayer_map_crash.asm
+++ src/singleplayer_objects_on_multiplayer_map_crash.asm
@@ -2,12 +2,13 @@
 %include "macros/datatypes.inc"
 %include "TiberianSun.inc"
 
-; 0062E6F6 - Do not spawn singleplayer objects in multiplayer games
+; 0x0062E6F6 - Do not spawn singleplayer objects in multiplayer games
+; 0x005DC279 - Fill_In_Data
 
 sstring Nod, "Nod"
 sstring GDI, "GDI"
 
-%macro ApplyFix 2 
+%macro ApplyFix 2 ; 0x0062E6F6
 hack %1
     add esp, 8
     mov ecx, eax
@@ -44,3 +45,36 @@ ApplyFix 0x004D7B95, 0x004D7F30 ; InfantryClass::Read_INI(CCINIClass &)
 ApplyFix 0x0043483E, 0x00434D64 ; BuildingClass::Read_INI(CCINIClass &)
 ApplyFix 0x0040E801, 0x0040EAAC ; AircraftClass::Read_INI(CCINIClass &)
 ;ApplyFix 0x, 0x ; 
+
+; 0x005DC279
+hack 0x0064A201, 0x0064A209 ;TriggerTypeClass::Read_INI(CCINIClass &)
+    call __strcmpi
+    add esp, 0x10
+    
+    cmp dword [SessionType], 0 ; no fix for singleplayer
+    jz hackend
+    pushad
+
+    push esi
+    push Nod
+    call __strcmpi
+    add esp, 8
+    test eax, eax
+    jz .skip
+    
+    push esi
+    push GDI
+    call __strcmpi
+    add esp, 8
+    test eax, eax
+    jz .skip
+    
+    popad
+    jmp hackend
+
+.skip:
+    popad
+    pop edi
+    pop esi
+    pop ebx
+    jmp 0x0064A4AE
