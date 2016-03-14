e7f9d59d649af227e315814865347cd1a956ccdc
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2016-03-14 14:13:41 +0100

    wrong CLEAR macro fix

--- src/no_movie_and_score_mix_dependency.asm
+++ src/no_movie_and_score_mix_dependency.asm
@@ -1,7 +1,7 @@
 %include "macros/patch.inc"
 %include "macros/datatypes.inc"
 
-@CLEAR 0x004E45D8, 0x004E45DB
+@CLEAR 0x004E45D8, 0x90, 0x004E45DB
 @SET 0x004E45D8, { mov al, 1 } ;Disable MoviesXX.mix check
 
 @SJMP 0x004E44A7, 0x004E44B6 ;Disable Scores.mix check
