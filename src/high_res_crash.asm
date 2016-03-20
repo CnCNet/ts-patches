57313b7ed81dfe25cde6a1d557fc3772ac758312
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2016-03-20 12:05:48 +0100

    reduce max sidebar icons to 16

--- src/high_res_crash.asm
+++ src/high_res_crash.asm
@@ -1,12 +1,12 @@
 %include "macros/patch.inc"
 %include "macros/datatypes.inc"
 
-; limit the height of the sidebar to max 1116 (20 icons). The game goes out of bounds with more icons and starts to corrupt memory
+; limit the height of the sidebar to max 862 (16 icons). The game goes out of bounds with more icons and starts to corrupt memory
 
 hack 0x005F2766
-    cmp eax, 1116
+    cmp eax, 862
     jbe .noChange
-    mov eax, 1116
+    mov eax, 862
     
 .noChange:
     mov dword[0x0074C24C], eax ; SidebarHeightStuff
@@ -14,9 +14,9 @@ hack 0x005F2766
 
 
 hack 0x005F60D7
-    cmp eax, 1116
+    cmp eax, 862
     jbe .noChange
-    mov eax, 1116
+    mov eax, 862
     
 .noChange:
     mov dword[0x0074C24C], eax ; SidebarHeightStuff
