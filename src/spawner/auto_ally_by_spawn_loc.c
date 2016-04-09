a464c5460f55e73f9a2605525081b3d02b9cf22c
Daniel Keeton Jr <dakeeton@gmail.com>
2016-04-09 01:40:18 -0500

    Comments added to auto_ally_by_spawn.c

--- src/spawner/auto_ally_by_spawn_loc.c
+++ src/spawner/auto_ally_by_spawn_loc.c
@@ -1,3 +1,14 @@
+/* file spawn.ini
+   [AllyBySpawnLocation]
+   A=0,1
+   B=2
+   C=3,4,5
+   D=7,6
+*/
+
+
+
+
 #include "macros/patch.h"
 #include "TiberianSun.h"
 #include "macros/utlist.h"
@@ -23,9 +34,13 @@ CALL(0x005DEF40,_ally_by_spawn_location);
 
 int32_t __thiscall
 ally_by_spawn_location(void *a1, int32_t a2, int32_t a3) {
+  // We're intercepting a call to Random2Class__operator(a1,a2,a3);
+  // just passing those args right through in the return statement of this function
+
   char *str_AllyBySpawn = "AllyBySpawnLocation";
   char buf[128] = {0};
   WWDebug_Printf("Starting Auto Allier\n");
+
   for (
        int i = INIClass__EntryCount(INIClass_SPAWN, str_AllyBySpawn);
        i-->0;
@@ -40,16 +55,21 @@ ally_by_spawn_location(void *a1, int32_t a2, int32_t a3) {
     int_ll *loc1, *loc2, *loc1_tmp, *loc2_tmp, *spawn_ll = NULL;;
     house_ll *house1, *house2, *h1_tmp, *h2_tmp;
 
+    // Parse the integer list from char *buf into spawn_ll
     ParseIntLL(buf, &spawn_ll);
 
+    /* This seems overly complicated but considering there can be more than one house
+     * at each spawn location and ::Make_Ally_House is a unidirectional function so it
+     * needs to be executed twice for a complete allyship
+     */
     LL_FOREACH_SAFE(spawn_ll, loc1, loc1_tmp) {
-      WWDebug_Printf("loc1->v = %d\n",loc1->v);
+
       LL_FOREACH_SAFE(spawn_ll, loc2, loc2_tmp) {
-        WWDebug_Printf("loc2->v = %d\n",loc2->v);
+
         LL_FOREACH_SAFE(spawn_locations[loc1->v], house1, h1_tmp) {
-          WWDebug_Printf("ding\n");
+
           LL_FOREACH_SAFE(spawn_locations[loc2->v], house2, h2_tmp) {
-            WWDebug_Printf("House = %x, %x\n",house1->house, house2->house);
+
             if (house1->house == house2->house)
               continue;
             HouseClass__Make_Ally_House(house1->house, house2->house);
@@ -59,6 +79,10 @@ ally_by_spawn_location(void *a1, int32_t a2, int32_t a3) {
     }
 
   }
+  // Could have some operator_delete (aka free()) here but it's not much memory
+  // spawn_locations[] and spawn_ll leak
+
+  // Finish the intercepted call
   return Random2Class__operator(a1,a2,a3);
 }
 
