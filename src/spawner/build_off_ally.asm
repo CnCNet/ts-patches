ec64ba1fe15e77cbad8993c5f881264b937093a2
AlexB <Alexander-B@gmx.de>
2016-11-04 02:02:03 +0100

    Fixed Build Off Ally feature. Registers edi and esi were used in Is_Mutual_Ally without restoring the previous values.

--- src/spawner/build_off_ally.asm
+++ src/spawner/build_off_ally.asm
@@ -17,14 +17,13 @@ Is_Mutual_Ally:
     mov edx, [esp+4]
     mov eax, [esp+8]
 
-    mov esi, [HouseClassArray_Vector] ; HouseClassArray
-    mov edi, [ecx+4*edx]
+    mov ecx, [HouseClassArray_Vector] ; HouseClassArray
+    mov ecx, [ecx+4*edx]
 
     push edx
     push eax
 
     push eax
-    mov ecx, edi
     call HouseClass__Is_Ally
     cmp al, 0
 
@@ -33,11 +32,10 @@ Is_Mutual_Ally:
 
     jz .Return_False
 
-    mov esi, [HouseClassArray_Vector] ; HouseClassArray
-    mov edi, [ecx+4*edx]
+    mov ecx, [HouseClassArray_Vector] ; HouseClassArray
+    mov ecx, [ecx+4*edx]
 
     push eax
-    mov ecx, edi
     call HouseClass__Is_Ally
     cmp al, 0
     jz .Return_False
