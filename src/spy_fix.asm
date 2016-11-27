63cdee4ed5fd59152018a6f02eaaed5baa0b2ff4
Daniel Keeton Jr <dakeeton@gmail.com>
2016-11-27 17:47:09 -0600

    Fix the problems where gdi spies reveal information to all gdi houses. This patch needs testing so it's not added to TS or any mods yet.

---
+++ src/spy_fix.asm
@@ -0,0 +1,31 @@
+%include "TiberianSun.inc"
+%include "macros/patch.inc"
+%include "macros/datatypes.inc"
+
+;;; BuildingClass::Update_Spied
+@SET 0x0043ADEF, {mov ecx, [eax+0x20]}
+
+;;; Update_Spied_Radar
+@SET 0x004C98F9, {mov ecx, [eax+0x20]}
+
+;;; MapClass::Sight_From
+@SET 0x00510D23, {mov ecx, [eax+0x20]}
+
+;;; BuildingClass::Update_Spied_Power
+@SET 0x004C3A33, {mov ecx, [ecx+0x20]}
+
+;;; BuildingClass::Draw_Overlays
+@CLEAR 0x00428A7B, 0x90, 0x00428A7E
+@SET 0x00428A73, {mov ecx, [edx+0x20]}
+@CLEAR 0x00428A3A, 0x90, 0x00428A3D
+@SET 0x00428A32, {mov ecx, [edx+0x20]}
+
+;;; BuildingClass::Captured
+@SET 0x0042F69D, {mov ecx, [ebp+0x20]}
+@SET 0x0042FBC2, {mov ecx, [ebp+0x20]}
+
+;;; Draw_Health_Boxes
+@CLEAR 0x0062C6E5, 0x90, 0x0062C6E8
+@SET 0x0062C6DD, {mov ecx, [edx+0x20]}
+@CLEAR 0x0062CA3D, 0x90, 0x0062CA40
+@SET 0x0062CA35, {mov ecx, [edx+0x20]}
