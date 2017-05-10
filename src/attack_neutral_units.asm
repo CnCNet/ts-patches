3ae5787d87f033b3ee362ae1ae42bedd736bdb99
Daniel Keeton Jr <dakeeton@gmail.com>
2017-05-09 23:57:43 -0500

    If a building has weapon range of 0 then don't consider it a threat.

--- src/attack_neutral_units.asm
+++ src/attack_neutral_units.asm
@@ -32,3 +32,25 @@ hack 0x0062D4B2, 0x0062D4BA
         jz      0x0062D8C0      ; Don't attack
 
         jmp     hackend
+
+
+@LJZ 0x0062D45C, _Dont_attack_any_buildings_with_no_weapon
+section .text
+_Dont_attack_any_buildings_with_no_weapon:
+        mov     eax, [esi]
+        push    0               ; Primary weapon
+        mov     ecx, esi
+        call    [eax+0x30C]
+
+        test    eax, eax
+        jz      0x0062D8C0      ; Don't attack
+
+        mov     eax, [eax]
+        test    eax, eax
+        jz      0x0062D8C0      ; Don't attack
+
+        mov     ecx, [eax+0x7C] ; Check for Range
+        test    ecx, ecx        ; Ranges is zero?
+        jnz     hackend
+
+        jmp     0x0062D8C0      ; Don't attack
