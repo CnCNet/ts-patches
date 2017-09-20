d7d90f80838dea4c3a9e3f07eaee9ff91ab5dec8
Daniel Keeton Jr <dakeeton@gmail.com>
2017-09-20 12:53:26 -0500

    Adding protocol_zero file

---
+++ src/spawner/protocol_zero.asm
@@ -0,0 +1,7 @@
+%include "TiberianSun.inc"
+%include "macros/patch.inc"
+%include "macros/datatypes.inc"
+
+cextern SpawnerActive
+@CLEAR 0x00508B0C, 0x90, 0x00508B19
+@CLEAR 0x005B1A2D, 0x90, 0x005B1A3B
