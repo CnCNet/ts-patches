881764d46c26adba58f860896691064ac34f2899
Rampastring <rampastring@outlook.com>
2017-11-13 20:53:51 +0200

    Fixed typo in max pip count hack

--- src/mods/dta/max_pip_counts.asm
+++ src/mods/dta/max_pip_counts.asm
@@ -3,9 +3,9 @@
 ; Harvester pip count 5 (including it here just so it can be changed easily)
 @SET 0x0063D48E, {mov eax, 5}
 
-; Change max passenger pip count to 8
-@SET 0x0063D487, {cmp ecx, 8}
+; Change max passenger pip count to 20
+@SET 0x0063D487, {cmp ecx, 20}
 
 ; Mobile EMP charge pip count?
-@SET 0x0064D494, {mov eax, 8}
+@SET 0x0063D494, {mov eax, 8}
 
