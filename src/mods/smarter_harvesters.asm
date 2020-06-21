4642db2be3a71db48187408b66db70283dd36a6c
Rampastring <rampastring@outlook.com>
2020-06-21 22:37:46 +0300

    Add harvester queue jumping

--- src/mods/smarter_harvesters.asm
+++ src/mods/smarter_harvesters.asm
@@ -11,7 +11,7 @@
 ; *******************
 ; Author: Rampastring
 
-; Dock building object pointers
+; Dock building object pointers and distances
 gint ClosestFreeRefinery, 0
 gint ClosestFreeRefineryDistance, 0
 gint ClosestPossiblyOccupiedRefinery, 0
@@ -74,7 +74,7 @@ hack 0x00654EEE
     push 2 ; RADIO_HELLO
     mov  ecx, esi
     call dword [edx+218h] ; RadioClass::Transmit_Message
-    cmp  eax, 1            ; Did the refinery reply with RADIO_ROGER?
+    cmp  eax, 1           ; Did the refinery reply with RADIO_ROGER?
     jne .Else
     ; The refinery accepted us, change status to HEADINGHOME and jump to original code (default case)
     mov  dword [esi+58h], 3 ; 3 = HEADINGHOME
@@ -163,4 +163,128 @@ Find_Closest_Refinery:
     pop  edx
     pop  ebx
     retn 8h
+
+
+; *****************************************************************************
+; Queue jumping code starts here
+
+gint OldHarvDistance, 0
+
+; Hack BuildingClass::Receive_Message
+; Disable generic check for returning RADIO_NEGATIVE if 
+; (!ScenarioInit && In_Radio_Contact() && Contact_With_Whom() != from), instead add 
+; a specific check for refineries only
+hack 0x0042694A
+    mov  ecx, esi
+    mov  eax, [ecx+0x220] ; get BuildingTypeClass instance
+    mov  al,  [eax+81Dh]  ; BuildingTypeClass.Refinery
+    cmp  al, 0
+    je   0x0042697B
+    mov  eax, [ScenarioInit]
+    test eax, eax
+    jne  .Check 
+    mov  eax, [esi+78h]
+    test eax, eax
+    je   .Check
+    cmp  eax, edi
+    jne 0x00426BFD
+    
+.Check:
+    jmp 0x00426962
+
+
+; Queue jumping, part 2
+; Hack refinery-specific code in BuildingClass::Receive_Message
+hack 0x00426A71
+    mov  eax, [ScenarioInit]
+    test eax, eax
+    jnz  0x0042707B ; Return RADIO_ROGER if ScenarioInit is set
+    
+    mov  eax, [esi+04Ah] ; Is_Something_Attached
+    test eax, eax
+    jnz  .Cont
+    
+    ; We're not free, ghost them by returning RADIO_STATIC
+    pop  edi
+    pop  esi
+    pop  ebx
+    mov  eax, 0
+    pop  ebx
+    add  esp, 44h
+    retn 0Ch
+    
+.Cont:
+    ; Clear old memory
+    xor  eax, eax
+    mov  [OldHarvDistance], eax
+
+    ; Get object we're in radio contact with
+    mov  eax, [esi+78h]
+    test eax, eax
+    jz   .Return_OK
+    
+    mov  ebx, eax ; Save reference to ebx
+    
+    ; Get distance to object we're in contact with
+    mov  ecx, esi         ; "this" pointer
+    push eax              ; Pointer to object in contact
+    call 0x00586CC0       ; ObjectClass::Get_XY_Distance_From
+    
+    mov  [OldHarvDistance], eax
+    
+    mov  ecx, esi         ; "this" pointer
+    push edi              ; Pointer to object who sent us the radio message
+    call 0x00586CC0       ; ObjectClass::Get_XY_Distance_From
+    
+    add  eax, 1660000h    ; "random" value, as before
+    cmp  eax, [OldHarvDistance]
+    jge  .Return_Negative
+    
+    ; Drop the old harvester and return RADIO_ROGER
+    mov  edx, [esi]
+    push ebx          ; old harvester
+    push 3            ; RADIO_OVER_OUT (hopefully so also in TS)
+    mov  ecx, esi
+    call dword [edx+218h] ; RadioClass::Transmit_Message
+    jmp .Return_OK
+    
+.Return_Negative:
+    jmp 0x0042696C  ; Returns RADIO_NEGATIVE and exits the function properly as specified by the calling convention
+
+.Return_OK:
+    jmp 0x004269BD  ; Returns RADIO_ROGER and exits the function properly as specified by the calling convention
+
+
+; Hack FootClass::Mission_Enter to make harvesters seek for a new refinery to
+; unload into when their existing refinery dumps them
+hack 0x004A49A3
+    mov  edx, [esi]
+    mov  ecx, esi
+    call [edx+2Ch] ; What_Am_I()
+    cmp  eax, 1    ; RTTI_UNIT
+    je   .Check_For_Harvester
+    jmp  .Original_Code
+    
+.Check_For_Harvester
+    mov  edx, esi
+    mov  eax, [edx+360h] ; UnitClass->Class
+    mov  cl, [eax+48Eh]  ; UnitTypeClass.Harvester
+    test cl, cl
+    je   .Original_Code
+    
+    ; We're a harvester, try to find a new refinery instead of going idle
+    mov  eax, [esi]       ; UnitClass::vtbl
+    mov  ecx, esi         ; Unit pointer
+    push 9                ; MISSION_HARVEST
+    call dword [eax+158h] ; MissionClass::Assign_Mission
+    jmp 0x004A49B1
+    
+.Original_Code:
+    ; Replace instructions that our jump destroyed
+    mov  edx, [esi]
+    push 1
+    push 0
+    mov  ecx, esi
+    call dword [edx+354h] ; Enter_Idle_Mode
+    jmp  0x004A49B1
 
