262238a20297657ce048fa98a18bc655978db15b
Johan Themen <johan_themen@hotmail.com>
2022-10-26 03:07:48 +0200

    Revert some filename changes and allow Settings.ini for Vinifera

--- src/dta_hacks.asm
+++ src/dta_hacks.asm
@@ -106,31 +106,38 @@
 ; Skip setting CD when loading multiplayer scenario
 @SJMP 0x005DB25E, 0x005DB288
 
+sstring str_SettingsINI, "Settings.ini"
+@SET 0x004E0605, push str_SettingsINI
+@SET 0x0058A146, push str_SettingsINI
+@SET 0x005ED7F3, push str_SettingsINI
+@SET 0x005EE47B, push str_SettingsINI
+@SET 0x00601093, push str_SettingsINI
+@SET 0x00687E07, push str_SettingsINI
+
 ;
 ; The following patches will not be included in Vinifera compatible builds.
 ;
 %ifndef VINIFERA
 
 sstring str_EnhanceINI, "ENHANCE.INI"
-sstring str_SoundsMIX, "SOUNDS.MIX"
+;sstring str_SoundsMIX, "SOUNDS.MIX"
 sstring str_DTALong, "Dawn of the Tiberium Age"
 sstring str_DTAGameWindow, "DTA (Game Window)"
 sstring str_LanguageDLLNotFound, "Language.dll not found, please start DTA.exe and click Save in the Options menu."
-sstring str_SettingsINI, "Settings.ini"
-sstring str_SoundsINI, "SOUNDS.INI"
+;sstring str_SoundsINI, "SOUNDS.INI"
 sstring str_ArtEINI, "ARTE.INI"
 sstring str_AIEINI, "AIE.INI"
 sstring str_CacheMIX, "CACHE.MIX"
-sstring str_Sounds01MIX, "SOUNDS01.MIX"
+;sstring str_Sounds01MIX, "SOUNDS01.MIX"
 sstring str_Isodes, "ISODES"
 sstring str_IsodesMIX, "ISODES.MIX"
-sstring str_SideMIX, "SIDE%02d.MIX"
+;sstring str_SideMIX, "SIDE%02d.MIX"
 sstring str_SideCDMIX, "SIDECD%02d.MIX"
 sstring str_MenuINI, "MENU.INI"
 sstring str_BriefingPCX, "BRIEFING.PCX"
 sstring str_BattleEINI, "BATTLEE.INI"
 sstring str_SidencMIX, "SIDENC%02d.MIX"
-sstring str_SideMIXRoot, "SIDE%02dE.MIX"
+;sstring str_SideMIXRoot, "SIDE%02dE.MIX"
 sstring str_MPMapsINI, "OBSOLETE.000"
 sstring str_MoviesMIX, "MOVIES.MIX"
 sstring str_DTAAlreadyRunning, "DTA is already running!"
@@ -138,8 +145,8 @@ sstring str_DTAAlreadyRunning, "DTA is already running!"
 sstring str_D1, "D1"
 sstring str_D2, "D2"
 sstring str_DTA, "DTA"
-sstring str_DesINI, "DES"
-sstring str_IsotemMIX, "ISOTEM"
+sstring str_DesINI, "DESERT"
+sstring str_IsotempMIX, "ISOTEMP"
 sstring str_DesertMIX, "DESERT"
 sstring str_TemperatMIX, "TEMPERAT"
 sstring str_Des, "DES"
@@ -153,31 +160,27 @@ sstring str_TemperatPAL, "TEMPERAT.PAL"
 
 ; String references
 @SET 0x00407081, push str_EnhanceINI ; push offset str_EnhanceINI?
-@SET 0x0044EBF3, push str_SoundsMIX
+;@SET 0x0044EBF3, push str_SoundsMIX
 @SET 0x00472567, push str_DTALong
 @SET 0x0047256C, push str_LanguageDLLNotFound
-@SET 0x004E0605, push str_SettingsINI
-@SET 0x004E0912, push str_SoundsINI
-@SET 0x004E0919, push str_SoundsINI
+;@SET 0x004E0912, push str_SoundsINI
+;@SET 0x004E0919, push str_SoundsINI
 @SET 0x004E1196, push str_ArtEINI
 @SET 0x004E11E6, push str_EnhanceINI
 @SET 0x004E1547, push str_AIEINI
 @SET 0x004E4078, push str_CacheMIX
-@SET 0x004E430F, push str_SoundsMIX ; Sounds01.MIX?
-@SET 0x004E4360, push str_SoundsMIX ; Sounds01.MIX?
-@SET 0x004E439C, push str_SoundsMIX
-@SET 0x004E43ED, push str_SoundsMIX
-@SET 0x004E80D8, push str_SideMIX
+;@SET 0x004E430F, push str_SoundsMIX ; Sounds01.MIX?
+;@SET 0x004E4360, push str_SoundsMIX ; Sounds01.MIX?
+;@SET 0x004E439C, push str_SoundsMIX
+;@SET 0x004E43ED, push str_SoundsMIX
+;@SET 0x004E80D8, push str_SideMIX
 @SET 0x004E838C, push esi
 @SET 0x004E8391, push str_SideCDMIX	; Search for SideCD##.mix instead of E01SC##.mix
 @SET 0x005801BB, push str_MenuINI
-@SET 0x0058A146, push str_SettingsINI
 @SET 0x005C04AF, {mov ecx, str_BriefingPCX}
 @SET 0x005C649C, push str_ArtEINI
 @SET 0x005D1B26, push str_ArtEINI
 @SET 0x005D6D45, push str_ArtEINI
-@SET 0x005ED7F3, push str_SettingsINI
-@SET 0x005EE47B, push str_SettingsINI
 @SET 0x005FF2C0, {cmp edx, str_DTALong}
 @SET 0x005FF2C8, push str_DTALong
 @SET 0x005FF2D9, push str_DTALong
@@ -185,7 +188,6 @@ sstring str_TemperatPAL, "TEMPERAT.PAL"
 @SET 0x005FF3B2, push str_DTALong
 @SET 0x005FF3C3, push str_DTALong
 @SET 0x005FF4EC, push str_DTALong
-@SET 0x00601093, push str_SettingsINI
 @SET 0x005EE8B0, push str_MPMapsINI
 @SET 0x005EEB82, push str_MPMapsINI
 @SET 0x0044ECC5, push str_MoviesMIX
@@ -196,15 +198,14 @@ sstring str_TemperatPAL, "TEMPERAT.PAL"
 @SET 0x0068621A, push str_DTALong
 @SET 0x006862BD, push str_DTALong
 @SET 0x006862C2, push str_DTALong
-@SET 0x00687E07, push str_SettingsINI
 
 @SET 0x006CA930, {db "DESERT",0,0,0,0}
-@SET 0x006CA940, {db "DES",0,0,0}
+@SET 0x006CA940, {db "DESERT",0,0,0}
 @SET 0x006CA94A, {db "ISODES",0}
 @SET 0x006CA954, {db "DES",0}
 @SET 0x006CA958, {db "TEMPERATE", 0,0,0,0}
-@SET 0x006CA968, {db "TEM", 0,0,0}
-@SET 0x006CA972, {db "ISOTEM", 0}
+@SET 0x006CA968, {db "TEMPERAT", 0,0,0}
+@SET 0x006CA972, {db "ISOTEMP", 0}
 @SET 0x006CA97C, {db "TEM", 0}
 
 ; Palette renames
