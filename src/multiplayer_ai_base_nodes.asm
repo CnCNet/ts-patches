1e938dfa637be0fa98ba3b7e640dd9f7b2df14c6
Rampastring <rampastring@outlook.com>
2023-02-02 02:38:03 +0200

    Fix crash when AI attempts to raise money with base nodes enabled

--- src/multiplayer_ai_base_nodes.asm
+++ src/multiplayer_ai_base_nodes.asm
@@ -21,6 +21,7 @@ hack 0x004C128F
 
     jmp  0x004C129D
 
+
 ; Hack AI base spacing stuff
 hack 0x004CB9CC
     push esi
@@ -35,4 +36,17 @@ hack 0x004CB9CC
 
 .Return_One:
     jmp  0x004CB9D2
+
+
+; Prevent AI from raising money, just like in singleplayer
+; (not doing this seems to result in crashes when
+;  the AI runs out of money with base nodes enabled)
+hack 0x004C08C5
+    cmp  dword [SessionType], ebx
+    jz   0x004C09AF
+
+    cmp  byte [UseMPAIBaseNodes], 0
+    jnz  0x004C09AF
+
+    jmp  0x004C08D1
 
