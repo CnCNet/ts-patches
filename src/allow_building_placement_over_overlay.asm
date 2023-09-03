7513993b57c5c60390c37e2d1ccfc7409e743788
Rampastring <rampastring@outlook.com>
2023-09-03 12:14:44 +0300

    Fix bug where the AI could not build over overlay that has Insignificant=yes

--- src/allow_building_placement_over_overlay.asm
+++ src/allow_building_placement_over_overlay.asm
@@ -34,3 +34,22 @@ hack 0x004525A2
     jnz  0x004525C8        ; this overlay is insignificant, don't let it affect building placement
     jmp  0x004522FB        ; return false
 
+
+; Hack BuildingTypeClass::Flush_For_Placement to allow placement over insignificant overlay
+; This is necessary for the AI. Human players do not hit this code path, instead the
+; CellClass::Is_Clear_To_Build hack above is enough for them
+hack 0x004401B9
+    mov  ecx, [eax+30h]  ; CellClass.Overlay, eax is a pointer to a CellClass instance
+    cmp  ecx, 0FFFFFFFFh ; if the overlay type is -1, then proceed as usual
+    je   0x004401DC
+
+    mov  esi, [0x007E22A4] ; DynamicVectorClass<OverlayTypeClass> entries
+                           ; we can safely use esi here as it's immediately
+                           ; re-assigned at 0x004401DC and also at the "bail out" code path
+
+    mov  edx, [esi+ecx*4]  ; fetch OverlayTypeClass instance from vector
+    cmp  byte [edx+0CBh], 1     ; ObjectTypeClass.IsInsignificant, if set, then proceed
+    je   0x004401DC
+
+    ; otherwise, if the overlay is not insignicant, proceed to the game's original WallTower-on-wall check
+    jmp  0x004401C1
