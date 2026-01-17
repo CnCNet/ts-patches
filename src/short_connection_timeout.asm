bb6dc126b4d422fa8e08cc7296482cbf088b39fd
Rampastring <rampastring@outlook.com>
2026-01-17 23:30:58 +0200

    Increase connection timeout
    With Vinifera, loading scenarios takes longer than with stock TSC so the existing timeout is sometimes too short

--- src/short_connection_timeout.asm
+++ src/short_connection_timeout.asm
@@ -2,5 +2,5 @@
 %include "macros/datatypes.inc"
 
 ;;; These values are 1/60th of a second.
-@SET 0x005DB794, {mov ecx, 1200}
-@SET 0x00707FC4, dd 0x4b0
+@SET 0x005DB794, {mov ecx, 2400}
+@SET 0x00707FC4, dd 0x960
